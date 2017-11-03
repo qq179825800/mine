6db6faffd80efec3f0a577d03bf8a5126b45148a
William <wxl3412@163.com>
2017-11-03 17:50:55 +0800

    添加加密文件系统

---
+++ file-encryption/src/main/java/cn/lin/fileencryption/FileEncryptionApplication.java
@@ -0,0 +1,12 @@
+package cn.lin.fileencryption;
+
+
+import javax.swing.*;
+
+public class FileEncryptionApplication {
+
+	public static void main(String[] args) {
+		JFrame jFrame = new SystemOS();
+		jFrame.setVisible(true);
+	}
+}
