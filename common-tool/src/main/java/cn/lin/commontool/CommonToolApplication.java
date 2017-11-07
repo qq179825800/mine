4563c2c22955712944b189867dd164c25fefe79d
William <wxl3412@163.com>
2017-11-07 14:52:09 +0800

    添加加密文件系统

---
+++ common-tool/src/main/java/cn/lin/commontool/CommonToolApplication.java
@@ -0,0 +1,25 @@
+package cn.lin.commontool;
+
+import cn.lin.code.util.SystemOS;
+
+import javax.swing.JFrame;
+
+/**
+ * Copyright (c)2017,德稻  All rights reserved.
+ *
+ * <core></core>
+ *
+ * @author wxl
+ *
+ * @version
+ *
+ */
+public class CommonToolApplication {
+
+	public static void main(String[] args) {
+		JFrame jFrame;
+		jFrame = new SystemOS();
+		jFrame.setVisible(true);
+
+	}
+}
