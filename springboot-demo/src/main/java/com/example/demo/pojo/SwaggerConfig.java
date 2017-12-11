e2381e91ce6cc612b34c75f04ab0ea5c564a4db0
William <wxl3412@163.com>
2017-12-11 12:32:21 +0800

    添加加密文件系统

--- springboot-demo/src/main/java/com/example/demo/pojo/SwaggerConfig.java
+++ springboot-demo/src/main/java/com/example/demo/pojo/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.pojo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -16,8 +17,8 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
-                .paths(regex("/user.*"))
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaData());
     }
@@ -32,4 +33,5 @@ public class SwaggerConfig {
                 "https://www.apache.org/licenses/LICENSE-2.0");
         return apiInfo;
     }
+
 }
