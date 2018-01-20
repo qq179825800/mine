7cbf6c979d4fab96eb3d103c079a360e9542cf88
william <wxl3412@163.com>
2018-01-20 23:15:35 +0800

    发送邮件

--- file-encryption/src/main/java/cn/lin/fileencryption/SendEmail.java
+++ file-encryption/src/main/java/cn/lin/fileencryption/SendEmail.java
@@ -7,10 +7,7 @@ import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.*;
 import java.io.File;
 import java.sql.Timestamp;
 import java.text.DateFormat;
@@ -56,6 +53,65 @@ public class SendEmail{
 		//发送邮件
 		Transport ts = session.getTransport();
 		ts.connect("wxl3412", "15884283609.+");
+		ts.sendMessage(msg, msg.getAllRecipients());
+	}
+	private void sendFileEmail() throws Exception{
+		Properties props = new Properties();//不设置任何配置，发送时需要
+		props.setProperty("mail.host", "smtp.163.com");
+		props.setProperty("mail.transport.protocol", "smtp");
+		props.setProperty("mail.smtp.auth", "true");//请求认证，与JavaMail的实现有关
+		Session session = Session.getInstance(props);
+		session.setDebug(true);
+		MimeMessage msg = new MimeMessage(session);
+		msg.setFrom(new InternetAddress("itheimacloud@163.com"));
+		msg.setRecipients(Message.RecipientType.TO, "itheima14@163.com");
+		msg.setSubject("JavaMail发送的邮件");
+
+		//邮件主体内容:组装过程
+		//文本部分
+		MimeBodyPart textPart = new MimeBodyPart();
+		textPart.setContent("美女aaa<img src='cid:mm'/>aaa", "text/html;charset=UTF-8");
+
+		//图片部分
+		MimeBodyPart imagePart = new MimeBodyPart();
+		//搞数据进来需要用到JAF的API
+		DataHandler dh = new DataHandler(new FileDataSource("src/1.jpg"));//自动探测文件的MIME类型
+		imagePart.setDataHandler(dh);
+		imagePart.setContentID("mm");
+
+		//附件部分
+		MimeBodyPart attachmentPart = new MimeBodyPart();
+		DataHandler dh1 = new DataHandler(new FileDataSource("src/姓名.zip"));//自动探测文件的MIME类型
+		String name = dh1.getName();
+		System.out.println(name);
+		attachmentPart.setDataHandler(dh1);
+		//手工设置附件的名称
+		attachmentPart.setFileName(MimeUtility.encodeText(name));
+
+		//描述关系：
+		MimeMultipart mmpart = new MimeMultipart();
+		mmpart.addBodyPart(textPart);
+		mmpart.addBodyPart(imagePart);
+		mmpart.setSubType("related");//有关系的
+
+
+		//把文本图片当做一个部分
+		MimeBodyPart textImagePart = new MimeBodyPart();
+		textImagePart.setContent(mmpart);
+
+		MimeMultipart multipart = new MimeMultipart();
+		multipart.addBodyPart(textImagePart);
+		multipart.addBodyPart(attachmentPart);
+		multipart.setSubType("mixed");//复杂关系
+
+
+		msg.setContent(multipart);
+
+		msg.saveChanges();
+//		msg.writeTo(new FileOutputStream("d:/3.eml"));
+		//发送邮件
+		Transport ts = session.getTransport();
+		ts.connect("itheimacloud", "iamsorry");
 		ts.sendMessage(msg, msg.getAllRecipients());
 	}
 }
